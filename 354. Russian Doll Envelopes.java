c68dab73391c81d25688f421a632f8ae733162b4
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2024-01-08 21:09:17 +0530

    Create 354. Russian Doll Envelopes.java

---
+++ 354. Russian Doll Envelopes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            if(a[0]==b[0]) {
+                return b[1]-a[1];
+            }
+            return a[0]-b[0];
+        });
+
+        int n = envelopes.length;
+        int[] dp = new int[n];
+        int len = 0;
+
+        for (int[] envelope : envelopes) {
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = envelope[1];
+            if (index == len) {
+                len++;
+            }
+        }
+
+        return len;
+    }
+}
